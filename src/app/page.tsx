acfa24124a1a84c2cab74bcf790eb9c29e0387ae
aashikur <mdaashikur@gmail.com>
2025-10-27 01:54:27 +0600

    shadCN button added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <div>
-      <h1 className="text-red-500">Hello LMS Team </h1>
+     <Button>Button</Button>
+     <Button variant="destructive">Button</Button>
+     <Button variant="outline">Button</Button>
+     <Button variant="secondary">Button</Button>
+     <Button variant="ghost">Button</Button>
+     <Button variant="link">Button</Button>
+     <Button variant="icon">Button</Button>
+     <Button variant="icon-sm">Button</Button>
     </div>
   );
 }
